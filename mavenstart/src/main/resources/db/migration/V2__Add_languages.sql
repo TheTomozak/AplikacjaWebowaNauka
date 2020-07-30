13f3922b8ca407266c0850e9aea7f6fc3bf5afe3
s18969 <tomaszpalczewski99@gmail.com>
2020-07-30 22:45:14 +0200

    Dodano hibernate, przy odpaleniu blędy

--- mavenstart/src/main/resources/db/migration/V2__Add_languages.sql
+++ mavenstart/src/main/resources/db/migration/V2__Add_languages.sql
@@ -1,4 +1,4 @@
 
 
 insert into languages (welcomeMsg, code) values ('Hello', 'en');
-insert into languages (welcomeMsg, code) values ('Siemanko', 'pl');
+insert into languages (welcomeMsg, code) values ('Siemka', 'pl');
