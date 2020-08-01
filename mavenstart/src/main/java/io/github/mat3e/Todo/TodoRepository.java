3917c2d0dd667a1e7122be4ebd757c9939f249d8
s18969 <tomaszpalczewski99@gmail.com>
2020-08-01 15:08:51 +0200

    Dodano odczyt zadan todo

---
+++ mavenstart/src/main/java/io/github/mat3e/Todo/TodoRepository.java
@@ -0,0 +1,19 @@
+package io.github.mat3e.Todo;
+
+import io.github.mat3e.HibernateUtil;
+
+import java.util.List;
+
+class TodoRepository {
+
+    List<Todo> findAll() {
+        var session = HibernateUtil.getSessionFactory().openSession();
+        var transaction = session.beginTransaction();
+
+        var result = session.createQuery("from Todo", Todo.class).list();
+
+        transaction.commit();
+        session.close();
+        return result;
+    }
+}
---
+++ mavenstart/src/main/java/io/github/mat3e/Todo/TodoServlet.java
@@ -0,0 +1,42 @@
+package io.github.mat3e.Todo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.mat3e.Hello.HelloServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Todo", urlPatterns = {"/api/todos/*"})
+public class TodoServlet extends HttpServlet {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(HelloServlet.class);
+
+    private TodoRepository repository;
+    private ObjectMapper mapper;
+
+    /**
+     * Servlet container needs it.
+     */
+    @SuppressWarnings("unused")
+    public TodoServlet() {
+        this(new TodoRepository(), new ObjectMapper());
+    }
+
+    TodoServlet(TodoRepository repository, ObjectMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LOGGER.info("Got request with parameters: " + req.getParameterMap());
+        resp.setContentType("application/json;charset=UTF-8");
+        mapper.writeValue(resp.getOutputStream(), repository.findAll());
+    }
+}
