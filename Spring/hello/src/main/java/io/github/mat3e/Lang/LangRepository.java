7e8f74ab2482a5ce1d573fb125ab7f73be619be8
TheTomozak <tomaszpalczewski99@gmail.com>
2020-08-03 15:49:43 +0200

    Dodanie spring data repository oraz dodanie adnotacji do serwisu

---
+++ Spring/hello/src/main/java/io/github/mat3e/Lang/LangRepository.java
@@ -0,0 +1,15 @@
+package io.github.mat3e.Lang;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LangRepository extends JpaRepository<Lang, Integer> {
+    List<Lang> findByCodeIsContaining(String codeFragment); // po nazwie metody spring potrafi zdefiniowac sql pod spodem
+    //ta metoda wyszuka wszytkie obiekty których code zawiera w sobie podaną literkę
+
+    List<Lang> findByCodeAndWelcomeMsg(String code, String welcomeMsg);
+
+}
